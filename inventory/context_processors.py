7920610ca83971f4de5c4a85ad825b6b192a6866
Brian Keifer <brian@valinor.net>
2017-01-29 22:21:40 -0500

    Add labels to nav template for car/loco counts

---
+++ inventory/context_processors.py
@@ -0,0 +1,32 @@
+#from datetime import date
+
+from django.db.models import Count
+from .models import Car, Locomotive
+
+def rollingStock_processor(request):
+    carCount = Car.objects.all().count()
+    locomotiveCount = Locomotive.objects.all().count()
+
+    return {
+        'carCount': carCount,
+        'locomotiveCount': locomotiveCount,
+    }
+# def season_processor(request):
+#     seasons = Season.objects.order_by('-year')
+#     seasonGroups = Race.objects.values('season_id').annotate(seasonracecount = Count('season_id'))
+#
+#     currentSeason = Season.objects.filter(year=date.today().year)
+#
+#     seasonRaceCount = {}
+#     for item in seasonGroups:
+#         seasonRaceCount[item['season_id']] = item['seasonracecount']
+#
+#     for season in seasons:
+#         if season.id not in seasonRaceCount:
+#             seasonRaceCount[season.id] = 0
+#
+#     return {
+#         'seasons':seasons,
+#         'seasonRaceCount':seasonRaceCount,
+#         'currentSeason':currentSeason[0].id,
+#     }
